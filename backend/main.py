67d699e42e4c2a5a47d51c591044b23f6deb7b40
Sadie Amato <saamato@email.unc.edu>
2023-04-03 09:40:54 -0400

    Add backend, frontend folders and basic skeleton of main.py

---
+++ backend/main.py
@@ -0,0 +1,19 @@
+"""The main file controlling the backend functionality of our website."""
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+### What the route does ###
+
+# fill in route @app...
+
+
+### What the route does ###
+
+# fill in route @app...
+
+### What the route does ###
+
+# fill in route @app...
