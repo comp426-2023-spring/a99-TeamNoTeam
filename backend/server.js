4bb708b50dfe7b6df726e4b89a20ea0734b12d68
Sadie Amato <saamato@email.unc.edu>
2023-04-05 18:26:58 -0400

    Add relevant imports and create user endpoint to server.js

--- backend/server.js
+++ backend/server.js
@@ -1,54 +1,45 @@
 // Main file controlling the backend functionality of our website
 
+// Import statements
+
+import sqlite3 from 'better-sqlite3';
+import express from 'express';
+import db from 'database.js';
 
 
+const app = express();
 
 
+// Endpoint creates user and adds it to the database
+app.post('/user/new/', (req, res, next) => {
+	
+    // Consolidate data from request
+    let userdata = {
+        name: req.body.name,
+		username: req.body.username,
+		password: req.body.password,
+		email: req.body.email,
+	}
 
-// ----------- Prelim stuff originally set up in main.py, copied here for reference as I work --------
+    // Insert a user with the correct info
 
-// from fastapi import FastAPI
+    // See if username is already in the database
+    const stmt1 = db.prepare(`SELECT * FROM users WHERE username='${userdata.username}'`);
+    let found_user = stmt1.get();
 
-// app = FastAPI()
+    // If the username is not already in the database, insert it
+    if (found_user === undefined) {
+        const stmt = `INSERT INTO users (name, username, password, email) VALUES ('${userdata.name}', '${userdata.username}', '${userdata.password}', '${userdata.email}');`;
+        db.exec(stmt)
+        res.status(200).json({"message": userdata.name + " created user " + userdata.username});
+        // TODO: Fill in what to render here when HTML files are created
+        // res.render();
 
-
-// ### What the route does ###
-
-// # fill in route @app...
-
-
-
-// ### Create new review based on user input ###
-
-// @app.post("/api/reviews")
-// def create_review():
-//     # create new review in the database
-//     # TODO: fill in params and return type 
-//     return
-
-
-// ### Get all reviews from the database ###
-
-// @app.get("/api/reviews")
-// def get_reviews():
-//     # TODO
-//     # returns a list of reviews from the database
-
-
-// ### Create new user based on user input ###
-
-// @app.post("/api/users")
-// def create_user():
-//     # TODO
-//     # adds a new user to the database
-
-
-// ### Deletes a user from the database ###
-
-// @app.delete("/api/users")
-// def delete_user():
-//     # TODO
-//     # deletes a user from the database
-
+    } else {
+        // If the user is already in the database and cannot be created
+        // TODO: Fill in what is rendered in this case
+        // res.render();
+    }
+});
 
 
