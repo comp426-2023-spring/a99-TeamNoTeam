5fc48fa6fe3d3f613080fe1f2402ee685ef1c243
Sadie Amato <saamato@email.unc.edu>
2023-04-05 17:33:21 -0400

    Delete main.py file, move its contents into server.js

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,54 @@
-// main file controlling the backend functionality of our website
+// Main file controlling the backend functionality of our website
+
+
+
+
+
+
+// ----------- Prelim stuff originally set up in main.py, copied here for reference as I work --------
+
+// from fastapi import FastAPI
+
+// app = FastAPI()
+
+
+// ### What the route does ###
+
+// # fill in route @app...
+
+
+
+// ### Create new review based on user input ###
+
+// @app.post("/api/reviews")
+// def create_review():
+//     # create new review in the database
+//     # TODO: fill in params and return type 
+//     return
+
+
+// ### Get all reviews from the database ###
+
+// @app.get("/api/reviews")
+// def get_reviews():
+//     # TODO
+//     # returns a list of reviews from the database
+
+
+// ### Create new user based on user input ###
+
+// @app.post("/api/users")
+// def create_user():
+//     # TODO
+//     # adds a new user to the database
+
+
+// ### Deletes a user from the database ###
+
+// @app.delete("/api/users")
+// def delete_user():
+//     # TODO
+//     # deletes a user from the database
+
+
+
